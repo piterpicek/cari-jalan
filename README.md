aa5381bffbe6ae46f4f63ab5304d9c0113e550ad
piterpicek <panda28.xyz@gmail.com>
2022-11-07 09:28:31 +0700

    Rev 2

---
+++ README.md
@@ -0,0 +1,30 @@
+# cari-jalan
+This tools for search postal code, address, region, valid maxmind/mindmax from IP Address and proxy.
+
+Note: `This tools is updated from build 2011 using php5.3 to php8.1`
+
+## Installation
+Download: `git clone https://github.com/piterpicek/cari-jalan.git`
+
+Requirements: `PHP 7.1+ and support PHP Curl`
+
+Feature:
+1. `Random UA`
+2. `API Check Proxy (Updated)`
+3. `API GMAP (Update)`
+4. `Accurate 78% for maxmind allowed`
+5. `customize and opensource`
+
+## Usage
+params: 
+1. `/ip/192.210.111.111` = `basic chacker`
+2. `/api/192.210.111.111` = `api output data search`
+
+## LOG
+1. `[2011] V1 php5.3 - bug 503 block request`
+2. `[2018] v1 revisi 7.1 - 503 api check proxy and update syntax`
+3. `[2021] revisi 2 stable - update syntax`
+
+## Disclaimer
+
+This tools for help easywork and not ilegal activity.
--- index.php
+++ index.php
@@ -1,32 +1,45 @@
 <?php
+########################
+# MR.Config - at 2011
+# rev 2. fb.me/mr.config
+# migrate 5.3 to php7+
+########################
 require_once 'lib/curl.php';
+// Google Maps Api
+if(!defined('gmap_address')) define('gmap_address', "https://maps.googleapis.com/maps/api/geocode/json" );
+if(!defined('gmap_key')) define('gmap_key', "AIzaSyBiszx96KFMqnD9OcN-ijDmTGYLqvsvhXA" );
+// Geo Plugins AHpOoo9SzNpm6HglASFo9cZ-GgP5E5f5WQ
+if(!defined('geoplugin')) define('geoplugin', "http://www.geoplugin.net/json.gp?ip=" );
+// Maxmind Api
+if(!defined('maxmind')) define('maxmind', "https://blackbox.ipinfo.app/lookup/" );
+
 if (isset($_GET['ip']) && !empty($_GET['ip'])) {
-    $dataku     = curl("http://www.geoplugin.net/json.gp?ip=".$_GET['ip']);
+    $dataku     = curl(geoplugin .$_GET['ip']);
     $out        = json_decode($dataku,true);
-    $cdetail    = curl("https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBiszx96KFMqnD9OcN-ijDmTGYLqvsvhXA&address=".$out['geoplugin_latitude']."+".$out['geoplugin_longitude']."&result_type=street_address&language=en");
+    $cdetail    = curl(gmap_address ."?key=". gmap_key ."&address=".$out['geoplugin_latitude']."+".$out['geoplugin_longitude']."&result_type=street_address&language=en");
     $lala       = json_decode($cdetail,true);
     echo $lala['results'][0]['formatted_address'];
     unlink('cook.tmp');
 }
 
 else if (isset($_GET['api']) && !empty($_GET['api'])) {
-    $dataku1    = curl("http://www.geoplugin.net/json.gp?ip=".$_GET['api']);
+    $dataku1    = curl(geoplugin .$_GET['api']);
     $out        = json_decode($dataku1,true);
     
-    $dataku2    = curl("http://proxy.mind-media.com/block/proxycheck.php?ip=".$_GET['api']);
-    if($dataku2 == 'Y') $resp = "Proxy Detected"; else $resp = "No Proxy Detected";
+    $dataku2    = curl(maxmind .$_GET['api']);
+    $maxmind    = ($dataku2 == 'Y') ? $resp = "Proxy Detected" : "No Proxy Detected";
 
-    $dataku3    = curl("https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBiszx96KFMqnD9OcN-ijDmTGYLqvsvhXA&address=".$out['geoplugin_latitude']."+".$out['geoplugin_longitude']."&result_type=street_address&language=en");
+    $dataku3    = curl(gmap_address ."?key=". gmap_key ."&address=".$out['geoplugin_latitude']."+".$out['geoplugin_longitude']."&result_type=street_address&language=en");
     $lala       = json_decode($dataku3,true);
-    if ($lala == "") $lalap = "<font color=\\\"red\\\">No Valid Mindmax</font>"; else $lalap = "<font color=\\\"green\\\">Valid Mindmax</font>";
+    $validate   = ($lala !== "" && $maxmind == "No Proxy Detected") ? "<font color=\\\"green\\\">Valid Mindmax</font>" : "<font color=\\\"red\\\">No Valid Mindmax</font>";
     
     header('Content-Type: application/json');
     echo '{
         "ip":"'.$out['geoplugin_request'].'",
         "country_id":"'.$out['geoplugin_countryCode'].'",
         "country":"'.$out['geoplugin_countryName'].'",
-        "city":"'.$out['geoplugin_city'].'",
-        "region":"'.$out['geoplugin_region'].'",
+        "city_geo":"'.$out['geoplugin_city'].'",
+        "region_geo":"'.$out['geoplugin_region'].'",
         "continent":"'.$out['geoplugin_continentName'].'",
         "timezone":"'.str_replace('\\','',$out['geoplugin_timezone']).'",
         "currency":"'.$out['geoplugin_currencyCode'].'/'.$out['geoplugin_currencySymbol'].'",
@@ -36,15 +49,17 @@ else if (isset($_GET['api']) && !empty($_GET['api'])) {
         "radius":"'.$out['geoplugin_locationAccuracyRadius'].'",
         "country":"'.$out['geoplugin_countryName'].'",
         "address":"'.$lala['results'][0]['formatted_address'].'",
-        "proxy":"'.$resp.'",
-        "valid_mindmax":"'.$lalap.'"
+        "proxy":"'.$maxmind.'",
+        "valid_mindmax":"'.$validate.'"
     }';
 
     unlink('cook.tmp');
 }
 
-/*{
-  "geoplugin_request":"192.210.134.111",
+/*
+OUTPUT DATA TESTER
+{
+  "geoplugin_request":"192.210.111.111",
   "geoplugin_status":200,
   "geoplugin_delay":"1ms",
   "geoplugin_credit":"Some of the returned data includes GeoLite data created by MaxMind, available from <a href='http:\/\/www.maxmind.com'>http:\/\/www.maxmind.com<\/a>.",
